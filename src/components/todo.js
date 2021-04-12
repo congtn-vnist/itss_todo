29a289c8d0a11d5f9a5be223037d8638effb91b1
TranNgocCong <55443845+TranNgocCong@users.noreply.github.com>
2021-04-12 09:14:33 +0700

    show list

---
+++ src/components/todo.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+ import {getKey} from "../lib/util";
+
+ function Todo() {
+   const [items, putItems] = React.useState([
+     
+     { key: getKey(), text: '日本語の宿題', done: false },
+     { key: getKey(), text: 'reactを勉強する', done: false },
+     { key: getKey(), text: '明日の準備をする', done: false },
+    
+   ]);
+
+   return (
+     <div className="panel">
+       <div className="panel-heading">
+         ITSS ToDoアプリ
+       </div>
+       {items.map(item => (
+         <label className="panel-block">
+             <input type="checkbox" />
+             {item.text}
+         </label>
+       ))}
+       <div className="panel-block">
+         {items.length} items
+       </div>
+     </div>
+   );
+ }
+
+ export default Todo; 
