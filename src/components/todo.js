1586daca247cd0f74680f85cea87890dc72dc227
EC2 Default User <ec2-user@ip-172-31-55-91.ec2.internal>
2021-05-20 08:53:15 +0000

    Save Todo on Firestore

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,10 +5,10 @@ import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
 
-import useStorage from '../hooks/useStorage';
+import useFirestore from '../hooks/useFirestore';
 
  function Todo() {
-    const [items, putItems, clearItems] = useStorage();
+    const [items, addItem, updateItem, clearItems] = useFirestore();
 
    const [filter, setFilter] = React.useState('ALL');
 
@@ -19,17 +19,11 @@ import useStorage from '../hooks/useStorage';
    });
 
    const handleCheck = checked => {
-    const newItems = items.map(item => {
-     if (item.key === checked.key) {
-       item.done = !item.done;
-     }
-     return item;
-   });
-   putItems(newItems);
+    updateItem(checked);
  };
 
    const handleAdd = text => {
-    putItems([...items, { key: getKey(), text, done: false }]);
+     addItem({ text, done: false });
   };
 
   const handleFilterChange = value => setFilter(value);
@@ -50,7 +44,7 @@ import useStorage from '../hooks/useStorage';
        />
        {displayItems.map(item => (
          <TodoItem 
-           key={item.key}
+           key={item.id}
            item={item}
            onCheck={handleCheck}
          />
